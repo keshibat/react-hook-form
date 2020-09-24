e51e0bb104bd8d7e249038c257ad35474de772b3
keshibat <kensukeshibata@gmail.com>
2020-09-24 12:00:53 +0900

    basic react-hook-form

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -4,44 +4,25 @@ import './SignUp.css';
 
 
 const SignUp = () => {
-  const { register, handleSubmit, errors} = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data)
+  const {handleSubmit, register, errors} = useForm();
+
+  const onSubmit = (formData) => {
+    console.log(formData);
   }
-
 
   return (
     <div>
       <h1>個人アカウント</h1>
       <form autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
         <label>ユーザーID</label>
-        <input type='text' name='userID' ref={register} />
-        {errors.username && <p>{errors.username.message}</p>}
+        <input
+          type='text'
+          name='userId'
+          ref={register({required: true, minLength: 5})}
+        />
+        {errors.userId && <p>UserIDは5文字以上</p>}
 
-        <label>姓</label>
-        <input type='text' name='userID' ref={register} />
-        {errors.username && <p>{errors.username.message}</p>}
-
-        <label>名</label>
-        <input type='text' name='userID' ref={register} />
-        {errors.username && <p>{errors.username.message}</p>}
-
-        <label>メール</label>
-        <input type='text' name='userID' ref={register} />
-        {errors.username && <p>{errors.username.message}</p>}
-
-        <label>パスワード</label>
-        <input type='password' name='password' ref={register} />
-        {errors.username && <p>{errors.username.message}</p>}
-
-        <label>地域</label>
-        <input type='number' name='region' ref={register} />
-
-        <span>
-          <input type='checkbox' name='remember' ref={register} />
-          <label>サービス利用規約に合意します</label>
-        </span>
 
         <button type='submit'>Submit</button>
       </form>
@@ -50,3 +31,24 @@ const SignUp = () => {
 }
 
 export default SignUp;
+
+
+      // <label>姓</label>
+      //   <input type='text' name='firstName' />
+
+      //   <label>名</label>
+      //   <input type='text' name='LastName' />
+
+      //   <label>メール</label>
+      //   <input type='text' name='email' />
+
+      //   <label>パスワード</label>
+      //   <input type='password' name='password' />
+
+      //   <label>地域</label>
+      //   <input type='number' name='region' />
+
+      //   <span>
+      //     <input type='checkbox' name='remember' ref={register} />
+      //     <label>サービス利用規約に合意します</label>
+      //   </span>
